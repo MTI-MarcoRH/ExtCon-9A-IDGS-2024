87450b77dd73ae02404c6eed64748c6cde9e9a68
Marco A. Ramirez <marco.ramirez@utxicotepec.edu.mx>
2024-07-18 22:17:33 -0600

    Add: Script de Tablas MATRICULA
    Se agregaron los script de creación de 1 tabla base: tbb_usuarios, 1 tabla catálogo: tbc_roles y 1 tabla derivada: tbd_usuarios_roles

--- respaldo_hospital_1807.sql
+++ respaldo_hospital_1807.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `hospital_general_9a_idgs2_210237` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `hospital_general_9a_idgs2_210237`;
+CREATE DATABASE  IF NOT EXISTS `hospital_general_9a_idgs` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hospital_general_9a_idgs`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hospital_general_9a_idgs
