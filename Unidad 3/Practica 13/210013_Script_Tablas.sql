d748d1eac7368eb8b896c555a15303078e5a7488
AlexisG <210013@utxicotepec.edu.mx>
2024-07-24 14:46:29 -0600

    correccion

--- Unidad 3/Practica 13/210013_Script_Tablas.sql
+++ Unidad 3/Practica 13/210013_Script_Tablas.sql
@@ -2,24 +2,20 @@
 -- ALEXIS GOMEZ
 -- Grado y Grupo: 9 A
 -- Ingenieria de Desarrollo  y Gestion de Software
- 
-
-
 
 CREATE TABLE tbc_servicios_medicos (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(255),
+    ID INT NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
     Descripcion TEXT,
     Observaciones TEXT,
-    Fecha_Registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Fecha_Registro DATETIME NOT NULL,
     Fecha_Actualizacion DATETIME
 );
 
 
-
 CREATE TABLE tbd_departamentos_servicios (
-    Departamento_ID INT NOT NULL,
-    Servicio_ID INT NOT NULL,
+    Departamento_ID INT UNSIGNED NOT NULL,
+    Servicio_ID INT UNSIGNED NOT NULL,
     Requisitos TEXT,
     Restricciones TEXT,
     Estatus BIT(1) NOT NULL DEFAULT b'1',
