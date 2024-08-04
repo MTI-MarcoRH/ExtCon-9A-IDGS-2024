3b31f369c00581debab00e6f59e4caece2e7f1ba
MyriamValderrabano <156869810+MyriamValderrabano@users.noreply.github.com>
2024-08-03 18:14:29 -0600

    Actualizacion del procedimiento dinámico

--- Unidad3/210467_script_poblacion_dinamica.sql
+++ Unidad3/210467_script_poblacion_dinamica.sql
@@ -1,3 +1,13 @@
+-- SCRIPT DEL PROCEDIMIENTO DINÁMICO DE LA TABLA 'tbd_lotes_medicamentos'
+
+-- ELABORADO POR: MYRIAM VALDERRABANO CORTES
+-- GRADO Y GRUPO: 9°
+-- PROGRAMA EDUCATIVO: INGENIERÍA EN DESARROLLO Y GESTIÓN DE SOFTWARE
+-- FECHA DE ELABORACIÓN: 03 DE AGOSTO DE 2024
+
+
+
+
 CREATE DEFINER=`myriam.valderrabano`@`%` PROCEDURE `sp_poblar_lotes_medicamentos_dinamico`(v_password VARCHAR(20), v_num_inserciones INT)
 BEGIN
 DECLARE max_medicamento_id INT;
@@ -6,10 +16,8 @@ DECLARE max_medicamento_id INT;
     IF v_password = "xYz$123" THEN
         
 
-        -- Obtener el último ID de tbc_medicamentos
         SELECT MAX(ID) INTO max_medicamento_id FROM tbc_medicamentos;
 
-        -- Obtener el último ID de tbb_personal_medico
         SELECT MAX(Persona_ID) INTO max_personal_medico_id FROM tbb_personal_medico;
 
         -- Bucle para insertar registros
