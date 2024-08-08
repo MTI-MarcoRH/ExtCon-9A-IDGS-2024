dc2de358f1441706df5bba662caef90174ce41a4
brayan <230893@utxicotepec.edu.mx>
2024-08-07 20:06:21 -0600

    Agregar notas a txt

---
+++ Unidad4/Practica14/230893_Poblacion_Dinamica.sql
@@ -0,0 +1,7 @@
+-- FUNCIONES DE TABLA ASIGNADA
+
+-- Elaborado por: Brayan Gutiérrez Ramírez
+-- Grado y Grupo:  9° A 
+-- Programa Educativo: Ingeniería de Desarrollo y Gestión de Software 
+-- Fecha elaboración:  5 de Agosto de 2024
+
