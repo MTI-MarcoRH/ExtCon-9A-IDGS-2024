b9856febc43a47b4b8e570578d8579019c0d2c79
Mando CV <210124@utxicotepec.edu.mx>
2024-08-15 09:47:13 -0600

    Añadir el sp estatico tbd_resultados_estudios

--- Unidad3/Practica13/210124_Poblacion.sql
+++ Unidad3/Practica13/210124_Poblacion.sql
@@ -29,3 +29,33 @@ FROM tbd_resultados_estudios re
 JOIN tbb_personas pac ON re.Paciente_ID = pac.ID
 JOIN tbb_personas pm ON re.Personal_Medico_ID = pm.ID
 JOIN tbc_estudios es ON re.Estudio_ID = es.ID;
+
+--5 sp estatico tbd_resultados_estudios
+DELIMITER $$
+CREATE DEFINER=`armando.carrasco`@`%` PROCEDURE `sp_poblar_resultados_estudios`(v_password VARCHAR(60))
+BEGIN
+    IF v_password="1234" THEN
+        INSERT INTO tbd_resultados_estudios (Paciente_ID, Personal_Medico_ID, Estudio_ID, Folio, Resultados, Observaciones, Estatus)
+        VALUES (8, 1, 2, UUID(), 'Resultados del primer estudio', 'Observaciones', 'En Proceso');
+        
+        INSERT INTO tbd_resultados_estudios (Paciente_ID, Personal_Medico_ID, Estudio_ID, Folio, Resultados, Observaciones, Estatus)
+        VALUES (6, 2, 1, UUID(), 'Resultados del segundo estudio', 'Observaciones', 'Completado');
+        
+        INSERT INTO tbd_resultados_estudios (Paciente_ID, Personal_Medico_ID, Estudio_ID, Folio, Resultados, Observaciones, Estatus)
+        VALUES (8, 3, 5, UUID(), 'Resultados del tercer estudio', 'Observaciones', 'Pendiente');
+        
+        INSERT INTO tbd_resultados_estudios (Paciente_ID, Personal_Medico_ID, Estudio_ID, Folio, Resultados, Observaciones, Estatus)
+        VALUES (5, 1, 2, UUID(), 'Resultados del cuarto estudio', 'Observaciones', 'En Proceso');
+        
+        UPDATE tbd_resultados_estudios 
+        SET Paciente_ID=5, Observaciones='Sin ningunas observaciones', Estatus='Completado'
+        WHERE ID=1;
+
+        DELETE FROM tbd_resultados_estudios 
+        WHERE ID=4;
+    ELSE 
+        SELECT "La contraseña es incorrecta, no se puede realizar modificación en la tabla Resultados Estudios" AS ErrorMessage;
+    END IF;
+END$$
+DELIMITER ;
+
