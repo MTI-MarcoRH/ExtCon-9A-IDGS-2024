815ded50b9b41b79b065d31570204aa2c2acf973
Diego <210436@utxicotepec.edu.mx>
2024-08-15 09:55:31 -0600

    Se creo la carpeta Unidad4 y se añadio el archivo sql de poblacion dinamica
    Se creo el procedimiento dinamico de manera basica :(

---
+++ Unidad4/poblacion_dinamica_210436.sql
@@ -0,0 +1,22 @@
+DELIMITER ;;
+
+CREATE DEFINER=`DiegoOliver`@`localhost` PROCEDURE `sp_poblar_horarios_dinamico`(v_password VARCHAR(20), IN v_especialidad VARCHAR(50))
+BEGIN
+    IF v_password = '1234' THEN
+        SET @sql = CONCAT('INSERT INTO tbd_horarios (empleado_id, nombre, especialidad, dia_semana, hora_inicio, hora_fin, turno, nombre_departamento, nombre_sala, fecha_creacion, fecha_actualizacion) VALUES ');
+        
+        -- Aquí podrías construir dinámicamente la parte de los valores basada en v_especialidad u otros parámetros
+        
+        SET @sql = CONCAT(@sql, '(1, \'Doctor A\', \'Cardiología\', \'Martes\', \'09:00:00\', \'17:00:00\', \'Matutino\', \'Departamento 1\', \'Sala 1\', \'2024-07-01 10:00:00\', \'2024-07-01 10:00:00\');');
+        
+        -- Ejecutar la consulta dinámica
+        PREPARE stmt FROM @sql;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+        
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Contraseña incorrecta';
+    END IF;
+END ;;
+
+DELIMITER ;
