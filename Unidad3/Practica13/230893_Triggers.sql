0a97b657ebd08bd76da62d2477edb6d4ef9806b5
brayan <230893@utxicotepec.edu.mx>
2024-08-17 23:26:02 -0600

    Actualización de triggers de tablas asignadas_ tbb_cirugias y tbd_cirugias_personal_medico

--- Unidad3/Practica13/230893_Triggers.sql
+++ Unidad3/Practica13/230893_Triggers.sql
@@ -3,10 +3,10 @@
 -- Elaborado por: Brayan Gutiérrez Ramírez
 -- Grado y Grupo:  9° A 
 -- Programa Educativo: Ingeniería de Desarrollo y Gestión de Software 
--- Fecha elaboración:  24 de Julio de 2024
+-- Fecha elaboración:  17 de Agosto de 2024
 
--- Triggers para la tabla cirugías
--- 1) AFTER INSERT CIRUGÍAS
+------------------ Triggers para la tabla cirugías --------------------
+---------- 1) AFTER INSERT CIRUGÍAS ---------------------------------------------
 DELIMITER &&
 CREATE DEFINER=`brayan.gutierrez`@`%` TRIGGER `tbb_cirugias_AFTER_INSERT` AFTER INSERT ON `tbb_cirugias` FOR EACH ROW BEGIN
 DECLARE v_paciente_nombre VARCHAR(155);
@@ -46,28 +46,25 @@ WHERE ID = NEW.Paciente_ID;
       'VALORACION MEDICA :',NEW.Valoracion_Medica,'\n',
       'CON ESTATUS: ',NEW.Estatus,'\n',
       'CONSUMIBLE: ',NEW.Consumible,'\n',
+      'FECHA PROGRAMACION CIRUGIA: ', NEW.Fecha_Programacion,'\n',
+	  'FECHA REALIZACION CIRUGIA: ', NEW.Fecha_Realizacion, '\n',
       'CON FECHA DE REGISTRO: ',NEW.Fecha_Registro,'\n'),
       DEFAULT,
-      DEFAULT);
+      DEFAULT);  
 END
-
-
 
 &&
 DELIMITER ;
 
 
--- 2) BEFORE UPDATE
-DELIMITER &&
+
+------------------------------------- 2) BEFORE UPDATE ----------------------------------
+
 CREATE DEFINER=`brayan.gutierrez`@`%` TRIGGER `tbb_cirugias_BEFORE_UPDATE` BEFORE UPDATE ON `tbb_cirugias` FOR EACH ROW BEGIN
-set new.fecha_actualizacion = current_timestamp();
+set new.Fecha_Actualizacion = current_timestamp();
 END
-&&
-DELIMITER ;
 
-
-
--- 3) AFTER UPDATE
+------------------------------------ 3) AFTER UPDATE ----------------------------------------------
 DELIMITER &&
 CREATE DEFINER=`brayan.gutierrez`@`%` TRIGGER `tbb_cirugias_AFTER_UPDATE` AFTER UPDATE ON `tbb_cirugias` FOR EACH ROW BEGIN
 DECLARE v_paciente_nombre_old VARCHAR(155);
@@ -124,17 +121,20 @@ concat_ws('', 'se ha modificado una cirugia con los siguientes datos: ',
 'VALORACIÓN MEDICA: ', old.Valoracion_Medica, '-', new.Valoracion_Medica,'\n',
 'ESTATUS: ', old.Estatus, '-', NEW.Estatus, '\n',
 'CONSUMIBLE: ', old.Consumible, '-' , NEW.Consumible,'\n',
+'FECHA PROGRAMACION CIRUGIA: ', old.Fecha_Programacion, '-', NEW.Fecha_Programacion, '\n',
+'FECHA REALIZACION CIRUGIA: ', old.Fecha_Realizacion, '-', NEW.Fecha_Realizacion, '\n',
 'CON FECHA DE REGISTRO: ', old.Fecha_Registro, '-', new.Fecha_Registro,'\n'
 ),
 default,
 default
 );
+
 END
 &&
 DELIMITER ;
 
 
--- 4) AFTER DELETE
+-------------------------- 4) AFTER DELETE --------------------------------------------
 DELIMITER &&
 
 CREATE DEFINER=`brayan.gutierrez`@`%` TRIGGER `tbb_cirugias_AFTER_DELETE` AFTER DELETE ON `tbb_cirugias` FOR EACH ROW BEGIN
@@ -170,12 +170,158 @@ insert into tbi_bitacora values(
     'VALORACIÓN MÉDICA: ', old.Valoracion_Medica,'\n',
     'ESTATUS: ', old.Estatus,'\n',
     'CONSUMIBLE: ', old.Consumible,'\n',
+    'FECHA PROGRAMACION CIRUGIA: ', old.Fecha_Programacion,'\n',
+    'FECHA REALIZACION CIRUGIA: ', old.Fecha_Realizacion,'\n',
 	'FECHA REGISTRO: ', old.Fecha_Registro,'\n',
     'FECHA ACTUALIZACIÓN: ', old.Fecha_Actualizacion
     ),
     default,
     default
     );
+
 END
+&&
+DELIMITER ;
+
+
+
+------------------ Triggers para la tabla cirugías personal medico  --------------------
+-------------------------------------- 1) AFTER INSERT ---------------------------------------------
+DELIMITER &&
+CREATE DEFINER=`brayan.gutierrez`@`%` TRIGGER `tbd_cirugias_personal_medico_AFTER_INSERT` AFTER INSERT ON `tbd_cirugias_personal_medico` FOR EACH ROW BEGIN
+ DECLARE v_estatus TINYINT DEFAULT 1;
+ DECLARE v_personal_medico_nombre VARCHAR(255);
+ declare v_cirugia varchar(50);
+ 
+ 
+    
+    SELECT CONCAT(Nombre, ' ', Primer_Apellido, ' ', COALESCE(Segundo_Apellido, '')) 
+    INTO v_personal_medico_nombre 
+    FROM tbb_personas 
+    WHERE ID = NEW.Personal_medico_ID;
+    
+	SELECT Tipo  INTO v_cirugia
+    FROM tbb_cirugias
+    WHERE ID = new.Cirugia_ID;
+
+ -- Inserta la información del nuevo registro en la tabla de bitacora
+    INSERT INTO tbi_bitacora 
+   VALUES (
+    DEFAULT,
+      CURRENT_USER(),
+	  'Create',
+	  'tbd_cirugias_personal_medico',
+	  concat_ws('', 'se ha creado una nueva CIRUGÍA con los siguientes datos: ',
+      'PERSONAL MÉDICO: ',v_personal_medico_nombre,'\n',
+      'CIRUGIA: ',v_cirugia,'\n',
+      'Con el rol de: ', NEW.Rol,'\n',
+      'ESTATUS: ', v_estatus,'\n'),
+      DEFAULT,
+      DEFAULT);
+
+
+
+END
+&&
+DELIMITER ;
+
+
+------------------------------------- 2) BEFORE UPDATE ----------------------------------
+DELIMITER &&
+CREATE DEFINER=`brayan.gutierrez`@`%` TRIGGER `tbd_cirugias_personal_medico_BEFORE_UPDATE` BEFORE UPDATE ON `tbd_cirugias_personal_medico` FOR EACH ROW BEGIN
+set new.Fecha_Actualizacion = current_timestamp();
+END
+&&
+DELIMITER ;
+
+
+------------------------------------ 3) AFTER UPDATE ----------------------------------------------
+DELIMITER &&
+CREATE DEFINER=`brayan.gutierrez`@`%` TRIGGER `tbd_cirugias_personal_medico_AFTER_UPDATE` AFTER UPDATE ON `tbd_cirugias_personal_medico` FOR EACH ROW BEGIN
+ DECLARE v_personal_medico_nombre_old VARCHAR(255);
+ DECLARE v_personal_medico_nombre_new VARCHAR(255);
+ declare v_cirugia_old varchar(50);
+ declare v_cirugia_new varchar(50);
+ 
+ 
+      -- Obtener nombres completos para Personal Médico y Paciente
+    SELECT CONCAT(Nombre, ' ', Primer_Apellido, ' ', COALESCE(Segundo_Apellido, ''))
+    INTO v_personal_medico_nombre_old 
+    FROM tbb_personas 
+    WHERE ID = OLD.Personal_medico_ID;
+	
+    SELECT CONCAT(Nombre, ' ', Primer_Apellido, ' ', COALESCE(Segundo_Apellido, '')) 
+    INTO v_personal_medico_nombre_new 
+    FROM tbb_personas 
+    WHERE ID = NEW.Personal_medico_ID;
+    
+    
+	SELECT Tipo  INTO v_cirugia_old
+    FROM tbb_cirugias
+    WHERE ID = OLD.Cirugia_ID;
+    
+	SELECT Tipo  INTO v_cirugia_new
+    FROM tbb_cirugias
+    WHERE ID = NEW.Cirugia_ID;
+ 
+ 
+insert into tbi_bitacora VALUES
+(
+DEFAULT,
+current_user(),
+'Update',
+'tbd_cirugias_personal_medico',
+concat_ws('', 'se ha modificado una CIRUGÍA con los siguientes datos: ',
+ 'PERSONAL MEDICO: ', v_personal_medico_nombre_old , '-' , v_personal_medico_nombre_new, '\n',
+ 'Cirugia: ', v_cirugia_old, '-', v_cirugia_new, '\n',
+ 'Con el rol de: ', OLD.Rol, '-', NEW.Rol, '\n',
+ 'Estatus: ',CAST(OLD.Estatus AS UNSIGNED), '-' ,CAST(NEW.Estatus AS UNSIGNED), '\n',
+ 'CON FECHA DE REGISTRO: ', old.Fecha_Registro, '-' , new.Fecha_Registro,'\n'
+),
+default,
+default
+);
+
+
+END
+
+&&
+DELIMITER ;
+
+------------------------------------ 4) AFTER DELETE --------------------------------------------
+DELIMITER &&
+CREATE DEFINER=`brayan.gutierrez`@`%` TRIGGER `tbd_cirugias_personal_medico_AFTER_DELETE` AFTER DELETE ON `tbd_cirugias_personal_medico` FOR EACH ROW BEGIN
+ DECLARE v_personal_medico_nombre_old VARCHAR(255);
+  declare v_cirugia_old varchar(50);
+  
+SELECT CONCAT(Nombre, ' ', Primer_Apellido, ' ', COALESCE(Segundo_Apellido, ''))
+INTO v_personal_medico_nombre_old 
+FROM tbb_personas 
+WHERE ID = OLD.Personal_medico_ID;
+    
+SELECT Tipo  INTO v_cirugia_old
+FROM tbb_cirugias
+WHERE ID = OLD.Cirugia_ID;
+
+insert into tbi_bitacora values(
+    default,
+    current_user(),
+    'Delete',
+    'tbd_cirugias_personal_medico',
+    concat_ws(' ','Se ha eliminado una CIRUGÍA con los siguientes datos: ',
+    'PERSONAL MÉDICO: ', v_personal_medico_nombre_old,'\n',
+    'CIRUGIA: ', v_cirugia_old, '\n',
+    "Con el Rol de: ", OLd.Rol, '\n',
+    'Estatus: ',CAST(OLD.Estatus AS UNSIGNED), '\n',
+	'FECHA REGISTRO: ', old.Fecha_Registro,'\n',
+    'FECHA ACTUALIZACIÓN: ', old.Fecha_Actualizacion
+    ),
+    default,
+    default
+    );
+
+
+END
+
 &&
 DELIMITER ;
