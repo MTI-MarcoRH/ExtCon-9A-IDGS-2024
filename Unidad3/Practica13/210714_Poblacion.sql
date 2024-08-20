320154657ca791b999bacdc437685fd00922e490
RommGG <117605051+RommGG@users.noreply.github.com>
2024-08-19 23:20:39 -0600

    Modificación del procedimiento almacenado de poblar valoraciones medicas
    Se ha modificado el procedimiento en base a la revisión.

--- Unidad3/Practica13/210714_Poblacion.sql
+++ Unidad3/Practica13/210714_Poblacion.sql
@@ -7,49 +7,29 @@
 
 -- Queries para revisión  y población de la tabla: Valoraciones Medicas
 
-CREATE DEFINER=`romualdo`@`%` PROCEDURE `sp_poblar_valoraciones_medicas`(v_password varchar(20))
+CREATE DEFINER=`romualdo`@`localhost` PROCEDURE `sp_poblar_valoraciones_medicas`(v_password VARCHAR(20))
 BEGIN
-IF v_password = "hola123" THEN
-	
+    IF v_password = "hola123" THEN
+        -- Inserciones en la tabla tbb_valoraciones_medicas
+        INSERT INTO hospital_general_9a_idgs_matricula.tbb_valoraciones_medicas (
+             Valor, Indicador, Unidad_medida, Paciente_id, Cita_id, Pm_id, Estatus, Fecha_registro
+        ) VALUES 
+        (36.5, 'Temperatura Corporal', 'Celsius', 1, 101, 1, 1, '2024-06-06'),
+        (120, 'Presión Arterial Sistólica', 'mmHg', 2, 102, 2, 1, '2024-06-07'),
+        (70, 'Frecuencia Cardíaca', 'bpm', 3, 103, 3, 1, '2024-06-07'),
+        (180, 'Altura', 'cm', 4, 104, 4, 1, '2024-06-07'),
+        (75, 'Peso Corporal', 'kg', 5, 105, 5, 1, '2024-06-07');
 
+        -- Actualización de un registro en tbb_valoraciones_medicas
+        UPDATE hospital_general_9a_idgs_matricula.tbb_valoraciones_medicas
+        SET Valor = 38.0, Indicador = 'Temperatura Corporal Actualizada'
+        WHERE ID = 1;
 
+        -- Eliminación de un registro en tbb_valoraciones_medicas
+        DELETE FROM hospital_general_9a_idgs_matricula.tbb_valoraciones_medicas
+        WHERE Paciente_id = 1;
 
-
-INSERT INTO tbb_valoraciones_medicas (
-	id, paciente_id, fecha, antecedentes_personales, antecedentes_familiares, antecedentes_medicos, sintomas_signos, examen_fisico, pruebas_diagnosticas,
-    diagnostico, plan_tratamiento, seguimiento) VALUES (1, 1,'2024-06-06','Sin antecedentes personales relevantes', 'Madre con diabetes tipo 2',
-    'Hipertensión arterial diagnosticada hace 5 años', 'Dolor abdominal, náuseas', 'Abdomen distendido, signo de Murphy positivo', 'Ecografía abdominal',
-    'Colecistitis aguda', 'Colecistectomía laparoscópica programada', 'Control postoperatorio en una semana');
-
-INSERT INTO tbb_valoraciones_medicas (
-	id, paciente_id, fecha, antecedentes_personales, antecedentes_familiares, antecedentes_medicos, sintomas_signos, examen_fisico, pruebas_diagnosticas,
-    diagnostico, plan_tratamiento, seguimiento) VALUES (2, 2, '2024-06-07', 'Fumador ocasional', 'Padre con hipertensión', 'Asma diagnosticada en la infancia',
-    'Tos persistente, dificultad para respirar', 'Sibilancias en ambos campos pulmonares', 'Espirometría', 'Asma bronquial', 'Tratamiento con broncodilatadores',
-    'Revisión en dos semanas');
-
-INSERT INTO tbb_valoraciones_medicas (
-    id, paciente_id, fecha, antecedentes_personales, antecedentes_familiares, antecedentes_medicos, sintomas_signos, examen_fisico, pruebas_diagnosticas,
-    diagnostico, plan_tratamiento, seguimiento) VALUES(3, 3, '2024-06-07', 'Deportista regular, sin antecedentes de tabaquismo', 'Madre con osteoporosis',
-    'Ninguno', 'Dolor en la rodilla derecha al correr', 'Inflamación en la rodilla derecha', 'Radiografía de rodilla', 'Tendinitis rotuliana', 'Fisioterapia y antiinflamatorios',
-    'Revisión en un mes');
-
-INSERT INTO tbb_valoraciones_medicas (
-id, paciente_id, fecha, antecedentes_personales, antecedentes_familiares, antecedentes_medicos, sintomas_signos, examen_fisico, pruebas_diagnosticas,
-    diagnostico, plan_tratamiento, seguimiento) VALUES (4, 4, '2024-06-07', 'Alergia a los mariscos', 'Hermano con asma', 'Alergias estacionales',
-    'Erupción cutánea y picazón después de comer mariscos', 'Erupciones eritematosas en brazos y piernas', 'Pruebas de alergia cutánea', 'Alergia alimentaria a mariscos',
-    'Antihistamínicos y evitar mariscos', 'Revisión en tres meses');
-
-UPDATE tbb_valoraciones_medicas
-SET plan_tratamiento = 'Nuevo plan de tratamiento'
-WHERE id = 1;
-
-
-DELETE FROM tbb_valoraciones_medicas
-WHERE paciente_id = 1;
-
-
-
-ELSE
-	select "La contraseña es incorrecta, no puedo mostrarte el estatus de la base de datos"  AS ErrorMessage;
-END IF;
+    ELSE
+        SELECT "La contraseña es incorrecta, no puedo mostrarte el estatus de la base de datos" AS ErrorMessage;
+    END IF;
 END
