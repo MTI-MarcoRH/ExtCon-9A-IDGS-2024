089d5457cb9caeb75f6e98b30ded61e547c1a72e
RommGG <117605051+RommGG@users.noreply.github.com>
2024-08-19 23:30:54 -0600

    Modificación de los triggers
    Modificación de los tiggers en base al cambio de los campos realizados en la tabla

--- Unidad3/Practica13/210714_Triggers.sql
+++ Unidad3/Practica13/210714_Triggers.sql
@@ -6,116 +6,90 @@
 -- Fecha elaboración:  23 de Julio de 2024
 
 
--- Triggers para la tabla de vValoraciones Medicas
+-- Triggers para la tabla de Valoraciones Medicas
 -- 1) AFTER INSERT Valoraciones Medicas
 CREATE DEFINER=`romualdo`@`localhost` TRIGGER `tbb_valoraciones_medicas_AFTER_INSERT` AFTER INSERT ON `tbb_valoraciones_medicas` FOR EACH ROW BEGIN
-    INSERT INTO tbi_bitacora (
+    INSERT INTO `hospital_general_9a_idgs_matricula`.`tbi_bitacora` (
         id,
         usuario,
         operacion,
         tabla,
         descripcion
     ) VALUES (
-        default,
-        current_user(),
+        DEFAULT,
+        CURRENT_USER(),
         'Create',
         'tbb_valoraciones_medicas',
-        concat_ws('', 
-            'Se ha registrado una nueva valoracion medica con los siguientes datos:', 
-            'Id: ', NEW.id,'\n',
-            'Paciente: ', NEW.paciente_id,'\n',
-            'Fecha: ', NEW.fecha,'\n',
-            'Antecedentes Personales: ', NEW.antecedentes_personales,'\n',
-            'Antecedentes Familiares: ', NEW.antecedentes_familiares,'\n',
-            'Antecedentes Medicos: ', NEW.antecedentes_medicos,'\n',
-            'Sintomas y Signos: ', NEW.sintomas_signos,'\n',
-            'Examen Fisico: ', NEW.examen_fisico,'\n',
-            'Pruebas Diagnosticas: ', NEW.pruebas_diagnosticas,'\n',
-            'Diagnostico: ', NEW.diagnostico,'\n',
-            'Plan de Tratamiento: ', NEW.plan_tratamiento,'\n',
-            'Seguimiento: ', NEW.seguimiento)
-            
-            
-            
-            
-            
+        CONCAT_WS('',
+            'Se ha registrado una nueva valoración médica con los siguientes datos:', '\n',
+            'ID: ', NEW.ID, '\n',
+            'Valor: ', NEW.Valor, '\n',
+            'Indicador: ', NEW.Indicador, '\n',
+            'Unidad de Medida: ', NEW.Unidad_medida, '\n',
+            'Paciente ID: ', NEW.Paciente_id, '\n',
+            'Cita ID: ', NEW.Cita_id, '\n',
+            'Pm ID: ', NEW.Pm_id, '\n',
+            'Estatus: ', NEW.Estatus, '\n',
+            'Fecha de Registro: ', NEW.Fecha_registro
+        )
     );
 END
-
 -- 2) BEFORE UPDATE
-CREATE DEFINER=`romualdo`@`localhost` TRIGGER `tbb_valoraciones_medicas_BEFORE_UPDATE` BEFORE UPDATE ON `tbb_valoraciones_medicas` FOR EACH ROW SET new.fecha_actualizacion = current_timestamp()
-
-
+CREATE DEFINER=`romualdo`@`localhost` TRIGGER `tbb_valoraciones_medicas_BEFORE_UPDATE` BEFORE UPDATE ON `tbb_valoraciones_medicas` FOR EACH ROW BEGIN
+    SET NEW.Fecha_actualizacion = CURRENT_TIMESTAMP();
+END
 -- 3) AFTER UPDATE
 CREATE DEFINER=`romualdo`@`localhost` TRIGGER `tbb_valoraciones_medicas_AFTER_UPDATE` AFTER UPDATE ON `tbb_valoraciones_medicas` FOR EACH ROW BEGIN
 
-    INSERT INTO tbi_bitacora (
+    INSERT INTO `hospital_general_9a_idgs_matricula`.`tbi_bitacora` (
         id,
         usuario,
         operacion,
         tabla,
         descripcion
     ) VALUES (
-        default,
-        current_user(),
+        DEFAULT,
+        CURRENT_USER(),
         'update',
         'tbb_valoraciones_medicas',
-        concat_ws('', 
-            'Se ha modificado al usuario con ID: ',old.id, "con los 
-        siguientes datos \n",
-			'Paciente: ', OLD.paciente_id, ' -> ', NEW.paciente_id, '\n',
-            'Fecha: ', OLD.fecha, ' -> ', NEW.fecha, '\n',
-            'Antecedentes Personales: ', OLD.antecedentes_personales, ' -> ', NEW.antecedentes_personales, '\n',
-            'Antecedentes Familiares: ', OLD.antecedentes_familiares, ' -> ', NEW.antecedentes_familiares, '\n',
-            'Antecedentes Medicos: ', OLD.antecedentes_medicos, ' -> ', NEW.antecedentes_medicos, '\n',
-            'Sintomas y Signos: ', OLD.sintomas_signos, ' -> ', NEW.sintomas_signos, '\n',
-            'Examen Fisico: ', OLD.examen_fisico, ' -> ', NEW.examen_fisico, '\n',
-            'Pruebas Diagnosticas: ', OLD.pruebas_diagnosticas, ' -> ', NEW.pruebas_diagnosticas, '\n',
-            'Diagnostico: ', OLD.diagnostico, ' -> ', NEW.diagnostico, '\n',
-            'Plan de Tratamiento: ', OLD.plan_tratamiento, ' -> ', NEW.plan_tratamiento, '\n',
-            'Seguimiento: ', OLD.seguimiento, ' -> ', NEW.seguimiento)
-            
-            
-            
-            
-            
+        CONCAT_WS('', 
+            'Se ha modificado el registro con ID: ', OLD.ID, ' con los siguientes datos \n',
+            'Valor: ', OLD.Valor, ' -> ', NEW.Valor, '\n',
+            'Indicador: ', OLD.Indicador, ' -> ', NEW.Indicador, '\n',
+            'Unidad de Medida: ', OLD.Unidad_medida, ' -> ', NEW.Unidad_medida, '\n',
+            'Paciente ID: ', OLD.Paciente_id, ' -> ', NEW.Paciente_id, '\n',
+            'Cita ID: ', OLD.Cita_id, ' -> ', NEW.Cita_id, '\n',
+            'Pm ID: ', OLD.Pm_id, ' -> ', NEW.Pm_id, '\n',
+            'Estatus: ', OLD.Estatus, ' -> ', NEW.Estatus, '\n',
+            'Fecha de Registro: ', OLD.Fecha_registro, ' -> ', NEW.Fecha_registro)
     );
 END
 
 -- 4) AFTER DELETE
 CREATE DEFINER=`romualdo`@`localhost` TRIGGER `tbb_valoraciones_medicas_AFTER_DELETE` AFTER DELETE ON `tbb_valoraciones_medicas` FOR EACH ROW BEGIN
-    INSERT INTO tbi_bitacora (
+    INSERT INTO `hospital_general_9a_idgs_matricula`.`tbi_bitacora` (
         id,
         usuario,
         operacion,
         tabla,
         descripcion
     ) VALUES (
-        default,
-        current_user(),
+        DEFAULT,
+        CURRENT_USER(),
         'Delete',
         'tbb_valoraciones_medicas',
-        concat_ws('', 
-            'Se ha eliminado una valoracion medica con los siguientes datos:', 
-            'Id: ', old.id,'\n',
-            'Paciente: ', old.paciente_id,'\n',
-            'Fecha: ', old.fecha,'\n',
-            'Antecedentes Personales: ', old.antecedentes_personales,'\n',
-            'Antecedentes Familiares: ', old.antecedentes_familiares,'\n',
-            'Antecedentes Medicos: ', old.antecedentes_medicos,'\n',
-            'Sintomas y Signos: ', old.sintomas_signos,'\n',
-            'Examen Fisico: ', old.examen_fisico,'\n',
-            'Pruebas Diagnosticas: ', old.pruebas_diagnosticas,'\n',
-            'Diagnostico: ', old.diagnostico,'\n',
-            'Plan de Tratamiento: ', old.plan_tratamiento,'\n',
-            'Seguimiento: ', old.seguimiento)
-            
-            
-            
-            
-            
+        CONCAT_WS('', 
+            'Se ha eliminado una valoración médica con los siguientes datos:', '\n', 
+            'ID: ', OLD.ID, '\n',
+            'Valor: ', OLD.Valor, '\n',
+            'Indicador: ', OLD.Indicador, '\n',
+            'Unidad de Medida: ', OLD.Unidad_medida, '\n',
+            'Paciente ID: ', OLD.Paciente_id, '\n',
+            'Cita ID: ', OLD.Cita_id, '\n',
+            'Pm ID: ', OLD.Pm_id, '\n',
+            'Estatus: ', OLD.Estatus, '\n',
+            'Fecha de Registro: ', OLD.Fecha_registro)
     );
 END
-
 
 
