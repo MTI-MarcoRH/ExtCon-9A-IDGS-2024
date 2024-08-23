57f599aad535a5d8b41cdb30cb050ef2a3a1a995
Daniela-AG15 <161864911+Daniela-AG15@users.noreply.github.com>
2024-08-23 11:38:56 -0600

    Script de llenado 200051_PoblacionDinamica.sql

--- Unidad4/200051_PoblacionDinamica.sql
+++ Unidad4/200051_PoblacionDinamica.sql
@@ -1,1 +1,16 @@
+TABLA DERIVADA CIRUGIA CONSUMIBLES 
 
+call sp_poblar_pacientes_dinamico('1234', 200);
+select * from tbb_pacientes;
+-- 2 poblar espacios medicos
+call sp_poblar_espacios('xYz$123');
+select*from tbc_espacios;
+-- 3 poblar cirugias
+call sp_poblar_cirugias_dinamico(200);
+select*from tbb_cirugias;
+-- 4 poblar consumibles
+call sp_poblar_consumibles('xYz$123');
+select*from consumibles;
+-- 5 cirugias consumibles
+call sp_poblar_cirugia_consumibles_dinamico('100');
+select * from tbd_cirugia_consumibles;
